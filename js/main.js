9be643bed6da3031b0645737d36d9f68e2175824
pakkapon phongtawee <pure1997@windowslive.com>
2013-11-26 06:30:20 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function fx_th2lne(){
+	document.getElementById('area_lne').value=document.getElementById('area_thai').value.lne();
+}
+function fx_lne2th(){
+	document.getElementById('area_thai').value=document.getElementById('area_lne').value.thai();
+}
